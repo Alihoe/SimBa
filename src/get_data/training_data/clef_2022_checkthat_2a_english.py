d95659d3712aa915eba15b4192eb306f4e0d47b9
alihoe <alica.hoevelmeyer@live.de>
2022-10-03 12:56:12 +0200

    added training data 2022

---
+++ src/get_data/training_data/clef_2022_checkthat_2a_english.py
@@ -0,0 +1,26 @@
+import requests
+from zipfile import ZipFile
+from pathlib import Path
+from os.path import join
+from os import listdir, rmdir
+from shutil import move
+
+from src.get_data import DATA_PATH
+
+
+def run():
+    directory = DATA_PATH + "clef_2022_checkthat_2a_english/training_data"
+    Path(directory).mkdir(parents=True, exist_ok=True)
+    queries = requests.get('https://gitlab.com/checkthat_lab/clef2022-checkthat-lab/clef2022-checkthat-lab/-/raw/main/task2/data/subtask-2a--english/CT2022-Task2A-EN-Train-Dev_Queries.tsv')
+    with open(directory+"/queries.tsv", 'wb') as f:
+        f.write(queries.content)
+    dev_qrels = requests.get('https://gitlab.com/checkthat_lab/clef2022-checkthat-lab/clef2022-checkthat-lab/-/raw/main/task2/data/subtask-2a--english/CT2022-Task2A-EN-Dev_QRELs.tsv')
+    with open(directory+"/dev_qrels.tsv", 'wb') as f:
+        f.write(dev_qrels.content)
+    train_qrels = requests.get('https://gitlab.com/checkthat_lab/clef2022-checkthat-lab/clef2022-checkthat-lab/-/raw/main/task2/data/subtask-2a--english/CT2022-Task2A-EN-Train_QRELs.tsv')
+    with open(directory+"/train_qrels.tsv", 'wb') as f:
+        f.write(train_qrels.content)
+
+
+if __name__ == "__main__":
+    run()
