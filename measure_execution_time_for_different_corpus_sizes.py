45d9823554efadfb5aac4d332c671233cef111d3
alihoe <alica.hoevelmeyer@live.de>
2022-10-06 15:39:44 +0200

    altered execution time script

--- measure_execution_time_for_different_corpus_sizes.py
+++ measure_execution_time_for_different_corpus_sizes.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 import shutil
 import subprocess
 import time
@@ -15,7 +16,15 @@ def run():
     sizes = [1000, 5000, 10000]
 
     for corpus_size in sizes:
-        print("Measuring time for corpus size of "+str(corpus_size))
+        # get the verified claims embeddings
+        subprocess.call(["python", "src/candidate_retrieval/semantic_retrieval_corpus_chunks.py", data, str(corpus_size),
+             "braycurtis", "--union_of_top_k_per_feature", "spearman", "50", "-sentence_embedding_models", "all-mpnet-base-v2", "princeton-nlp/sup-simcse-roberta-large", "sentence-transformers/sentence-t5-base", "https://tfhub.dev/google/universal-sentence-encoder/4"])
+        for fname in os.listdir("data/corpus_chunks/"+ data):
+            if fname.startswith("embedded_queries"):
+                os.remove(os.path.join("data/corpus_chunks/"+ data, fname))
+
+        # Start measuring time here
+        print("Measuring time for corpus size of " + str(corpus_size))
         start_time = time.time() # Measure time for retrieval and re-ranking
         subprocess.call(["python", "src/candidate_retrieval/semantic_retrieval_corpus_chunks.py", data, str(corpus_size), "braycurtis", "--union_of_top_k_per_feature", "spearman", "50"])
         subprocess.call(["python", "src/re_ranking/multi_feature_re_ranking_corpus_chunks.py", data, "braycurtis", "spearman", "50"])
@@ -24,8 +33,8 @@ def run():
         # Don't measure exceution time for that
         subprocess.call(["python", "evaluation/scorer/main.py", data, "--use_corpus_chunk_data"])
         # Delete produced files
-        shutil.rmtree("data/corpus_chunks/"+ data, ignore_errors=False, onerror=None)
-    shutil.rmtree("data/corpus_chunks/", ignore_errors=False, onerror=None)
+        #shutil.rmtree("data/corpus_chunks/"+ data, ignore_errors=False, onerror=None)
+
 
 
 if __name__ == "__main__":
